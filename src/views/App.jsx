4427df526adbe6d44abc4a3e1ea5decd4114c8bd
minhllk24 <minhllk24406h@st.uel.edu.vn>
2026-05-29 22:58:25 +0700

    feat: Add In-Store Checkout page UI based on Figma design

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import GroomingSpaPage from "../pages/GroomingSpaPage";
+import CheckoutPage from "../pages/CheckoutPage";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       {/* Cả 2 route để phòng ngừa lỗi gõ sai chữ 'n' trong link Navbar Figma */}
       <Route path="/services/groomng-spa" element={<GroomingSpaPage />} />
       <Route path="/services/grooming-spa" element={<GroomingSpaPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
